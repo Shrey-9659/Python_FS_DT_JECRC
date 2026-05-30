232075abac4d4acd0bdd559e18c3f48a94542104
shrey-dev-2023 <136662917+shrey-dev-2023@users.noreply.github.com>
2026-05-30 19:33:08 +0530

    Lecture 7 : Django started - Dynamically changing data in html template

---
+++ Django/firstProject/myproject/myproject/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("This is home page")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("<h1>This is my about page</h1>")
+    return render(request, "about.html")
+
+def contact(request):
+    data = {
+        "email" : "shreyyyyyy@gmail.com",
+        "contact" : "+91 9660023360"
+    }
+    # return HttpResponse("This is contact us page")
+    return render(request, "contact.html", data)
