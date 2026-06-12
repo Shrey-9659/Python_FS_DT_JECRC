5f62d8c8730c80af8f19dfb9ae30726896e232a1
shrey-dev-2023 <136662917+shrey-dev-2023@users.noreply.github.com>
2026-06-12 10:59:30 +0530

    Datatypes : String, number

--- JavaScript/introduction.js
+++ JavaScript/introduction.js
@@ -42,12 +42,12 @@
 // let undefinedValue = undefined
 // console.log(undefinedValue)
 // var,let, const
-let stringValue = "Shrey"
-let intValue = 5
-let bigIntValue = 12345678912345678912345678n
-let bool = true
-let nullValue = null
-let undefinedValue;
+// let stringValue = "Shrey"
+// let intValue = 5
+// let bigIntValue = 12345678912345678912345678n
+// let bool = true
+// let nullValue = null
+// let undefinedValue;
 
 // console.log(typeof(stringValue))
 // console.log(typeof intValue)
@@ -66,3 +66,74 @@ let undefinedValue;
 // console.log(5 == 5)
 // console.log(5 == "5") -- equality check
 // console.log(5 === "5") -- strict equality check
+
+// let arr = [1,2,3,4,5, null, true, "String"]
+// console.log(arr)
+
+// let obj = {
+//     name : "Shrey",
+//     year : 2026,
+//     univ : "JECRC"
+// }
+
+// console.log(obj)
+
+// Conditionals
+// if(condition){
+    // login
+// }else if(condition){
+    // login
+// }else{
+    // logic
+// }
+
+// Age - less than 18 
+// age - 18 - 60 Eligible
+// Else : Not eligible
+// && -- Logical AND
+// || -- Logical OR
+// != -- Logical NOT
+
+let arr = [1,2,3,4,5,6,7]
+// FOR LOOP
+// for(initialization; condition; increment/decrement){
+//     logic
+// }
+
+// for(i = 0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// WHILE LOOP
+// while(condition){
+    // logic
+    // increment/decrement
+// }
+
+let i = 0
+// while(i<arr.length){
+//     console.log(arr[i])
+//     i++
+// }
+
+// do{
+    // logic
+// }while(condition)
+
+// do{
+//     console.log(arr[i])
+//     i++;
+// }while(i > arr.length)
+
+
+// let str = new String("Shrey")
+
+// console.log(str)
+
+let num = 123.45678
+// console.log(num)
+// console.log(num.toFixed(3))
+// console.log(num.toPrecision(2))
+
+
+console.log(typeof num.toString())
